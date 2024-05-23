4ab3090f1f06ba2dbdd2092b1a4055175e144c2e
Sigmanificient <edhyjox@gmail.com>
2024-05-23 14:47:15 +0200

    meson: replace hardcoded python command

--- pkgs/by-name/me/meson/package.nix
+++ pkgs/by-name/me/meson/package.nix
@@ -118,7 +118,7 @@ python3.pkgs.buildPythonApplication rec {
     ''test cases/osx/7 bitcode''
   ])
   ++ [
-    ''HOME="$TMPDIR" python ./run_project_tests.py''
+    ''HOME="$TMPDIR" ${python3.executable} ./run_project_tests.py''
     "runHook postCheck"
   ]);
 
