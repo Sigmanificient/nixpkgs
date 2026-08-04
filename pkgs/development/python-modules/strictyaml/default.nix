220de17e0c63bc1ee3e6c74d8a2658ba3458aad2
Sigmanificient <edhyjox@gmail.com>
2026-08-05 00:00:09 +0200

    python3Packages.strictyaml: migrate to pyproject

--- pkgs/development/python-modules/strictyaml/default.nix
+++ pkgs/development/python-modules/strictyaml/default.nix
@@ -4,12 +4,13 @@
   fetchPypi,
   ruamel-yaml,
   python-dateutil,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "strictyaml";
   version = "1.7.3";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -21,7 +22,9 @@ buildPythonPackage rec {
       --replace "ruamel.yaml==0.17.4" "ruamel.yaml"
   '';
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     ruamel-yaml
     python-dateutil
   ];
